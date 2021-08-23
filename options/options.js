dffa30c88ba124b5b4a34b968a264e6fbda2e989
Crowdbotics <team@crowdbotics.com>
2021-08-23 09:11:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "talentedu_30040Identifier",
+  url: "https://talentedu_30040Identifier.botics.co",
+  api: "https://talentedu_30040Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
